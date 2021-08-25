606c1993f0f167185c84bacb3b8ab3a7a2a0243f
Merrek DeBolt <itsugamer@gmail.com>
2021-08-25 15:53:39 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the input string? ");
+        String input = scanner.nextLine();
+        System.out.println(input + " has " + input.length() + " characters.");
+    }
+}
